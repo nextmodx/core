713f101656b203cce9b60cdb368bbea9d431a020
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-08-08 15:46:38 +0300

    Init config

---
+++ src/modx/Experiment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace modx\core;
+
+/**
+ * Class Experiment
+ * @package nextmodx\core
+ */
+class Experiment
+{
+    public function __construct()
+    {
+        echo self::class, PHP_EOL;
+    }
+}
